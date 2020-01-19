d7ee13d5e7eea6d618f005e0bc3df5fe06522bd8
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2020-01-19 08:41:55 -0300

    fontend web finisihing

---
+++ semana-omnistack/semana-10.0/web/src/App/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import api from '../Services/Api';
+
+import DevItems from '../components/DevItms';
+import DevForm from '../components/DevForm';
+
+import '../Styles/Global.css';
+import '../App/App.css';
+import '../Styles/SideBar.css';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    const loadDev = async () => {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+    loadDev();
+  }, [])
+
+  const handleAddDev = async (data) => {
+
+    const response = api.post('/devs', data);
+   
+    setDevs([...devs, response.data]);
+  }
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItems key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
