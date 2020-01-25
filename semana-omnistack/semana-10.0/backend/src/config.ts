ab2b6a7e2509305d6c8ca71af32703e55591c066
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2020-01-25 17:14:38 -0300

    starting backend development

--- semana-omnistack/semana-10.0/backend/src/config.ts
+++ semana-omnistack/semana-10.0/backend/src/config.ts
@@ -2,3 +2,7 @@ require("dotenv").config();
 
 export const PORT = process.env.PORT || "3333";
 export const HOST = process.env.HOST || "http://12.0.0.1";
+export const DB_NAME = process.env.DB_NAME || "omnistack";
+export const DB_USER = process.env.DB_USER || "omnistack_user";
+export const DB_PASS = process.env.DB_PASS || "OmnistackUser!";
+export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
--- semana-omnistack/semana-10.0/backend/src/server.ts
+++ semana-omnistack/semana-10.0/backend/src/server.ts
@@ -1,9 +1,16 @@
 import app from "./app";
+import { sequelize } from "./db";
 
 const PORT = process.env.PORT || "3333";
 
 (
     async () => {
+        try {
+            console.log(sequelize.config);
+            await sequelize.sync({ force: false });
+        } catch (err) {
+            console.log(`[error]: DB_ERROR failed to connect on database: `, err.stack);
+        }
         app.listen(PORT, () => {
             console.log(`[info]: Connected at host: http://127.0.0.1:${PORT}`);
         })
